80046298ce05b33169ff874065eb8a24d7c19615
Nick Mosher <nicholastmosher@gmail.com>
2016-02-23 02:10:28 -0500

    Added tab views and fragments to main activity.

--- mobile/src/main/java/com/example/svaswani/csh_automation_android/AutomationActivity.java
+++ mobile/src/main/java/com/example/svaswani/csh_automation_android/AutomationActivity.java
@@ -1,13 +1,18 @@
 package com.example.svaswani.csh_automation_android;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class AutomationActivity extends AppCompatActivity {
 
@@ -15,17 +20,38 @@ public class AutomationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_automation);
+
+        // Setup toolbar
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-//        fab.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-//            }
-//        });
+        // Setup viewPager which holds child fragments.
+        final ViewPager viewPager = (ViewPager) findViewById(R.id.view_pager);
+        ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
+
+        //Add the fragments to the viewPager.
+        viewPagerAdapter.addFragment(new GatekeeperFragment(), "Gatekeeper");
+        viewPagerAdapter.addFragment(new LoungeFragment(), "Lounge");
+        viewPager.setAdapter(viewPagerAdapter);
+
+        // Setup tabLayout which displays section tabs.
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tab_layout);
+        tabLayout.setupWithViewPager(viewPager); //Links the tabs to the views.
+        tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+
+                //Tells the viewPager to navigate to the position of the selected tab.
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) { }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) { }
+        });
     }
 
     @Override
@@ -37,6 +63,7 @@ public class AutomationActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
@@ -48,5 +75,47 @@ public class AutomationActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * This adapter serves fragments to the ViewPager to display when
+     * each tab on the toolbar is activated.
+     */
+    class ViewPagerAdapter extends FragmentPagerAdapter {
+
+        private final List<Fragment> mFragmentList = new ArrayList<>();
+        private final List<String> mFragmentTitles = new ArrayList<>();
+
+        public ViewPagerAdapter(FragmentManager manager) {
+            super(manager);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return mFragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragmentList.size();
+        }
+
+        /**
+         * Adds a fragment to this ViewPager adapter to display on the ViewPager.
+         * @param fragment The fragment to add.
+         * @param title The title of the fragment to display in the TabLayout
+         */
+        public void addFragment(Fragment fragment, String title) {
+            if(fragment == null || title == null) {
+                throw new NullPointerException("Fragment or fragment title is null.");
+            }
+            mFragmentList.add(fragment);
+            mFragmentTitles.add(title);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mFragmentTitles.get(position);
+        }
     }
 }
